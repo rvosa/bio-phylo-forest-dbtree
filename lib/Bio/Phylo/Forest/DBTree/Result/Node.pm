f2c7ffbf4845e503a2d45014b9a19b7b644760bd
rvosa <rutgeraldo@gmail.com>
2017-02-04 15:56:07 +0100

    added POD

--- lib/Bio/Phylo/Forest/DBTree/Result/Node.pm
+++ lib/Bio/Phylo/Forest/DBTree/Result/Node.pm
@@ -11,43 +11,55 @@ my $log = Bio::Phylo::Util::Logger->new;
 
 __PACKAGE__->table("node");
 
-=head1 ACCESSORS
+=head1 NAME
 
-=head2 id
+Bio::Phylo::Forest::DBTree::Result::Node - Phylogenetic database record as a node object
 
-  data_type: 'int'
-  is_nullable: 0
+=head1 SYNOPSIS
 
-=head2 parent
+ # same API is Bio::Phylo::Forest::Node
+ 
+=head1 DESCRIPTION
 
-  data_type: 'int'
-  is_nullable: 0
+This package implements an object-relational interface to records in a phylogenetic 
+database. This way, the record can be used as if it's a tree node with the same 
+programming interface as is used by L<Bio::Phylo>, but without making the demands that
+loading an entire tree in memory would make.
 
-=head2 left
+=head1 DATABASE ACCESSORS
 
-  data_type: 'int'
-  is_nullable: 0
+The following methods directly access fields of the database-backed node record. In 
+principle, these methods can also be used as setters, although for the indexes and keys
+you really need to know what you're doing or the topology of the tree could become 
+irreparably corrupted.
 
-=head2 right
+=head2 id()
 
-  data_type: 'int'
-  is_nullable: 0
+Returns primary key of the node object, an integer.
 
-=head2 name
+=head2 parent()
 
-  data_type: 'string'
-  is_nullable: 0
+Returns the foreign key of the node object's parent, an integer.
 
-=head2 length
+=head2 left()
 
-  data_type: 'float'
-  is_nullable: 0
+Returns the pre-order index of the node object, an integer.
 
-=head2 height
+=head2 right()
 
-  data_type: 'float'
-  is_nullable: 0
+Returns the post-order index of the node object, an integer.
 
+=head2 name()
+
+Returns the node name, a string.
+
+=head2 length()
+
+Returns the node's branch length, a floating point number.
+
+=head2 height()
+
+Returns the node's height from the root, a floating point number.
 
 =cut
 
@@ -77,10 +89,27 @@ sub _schema {
 	return $schema;
 }
 
+=head1 NODE METHODS
+
+These methods override methods of the same name in L<Bio::Phylo>, to make more efficient
+use of the database.
+
+=head2 get_parent()
+
+Returns the node's parent, if any.
+
+=cut
+
 sub get_parent {
 	my $self = shift;
 	return $self->_schema->find($self->parent);
 }
+
+=head2 get_children_rs()
+
+Returns the node's children, if any, as a L<DBIx::Class> result set.
+
+=cut
 
 sub get_children_rs {
 	my $self = shift;
@@ -93,7 +122,19 @@ sub get_children_rs {
 	});
 }
 
+=head2 get_children()
+
+Returns the node's children, if any, as an array reference.
+
+=cut
+
 sub get_children { [ shift->get_children_rs->all ] }
+
+=head2 get_descendants_rs()
+
+Returns the node's descendants, if any, as a L<DBIx::Class> result set.
+
+=cut
 
 sub get_descendants_rs {
 	my $self = shift;
@@ -107,7 +148,19 @@ sub get_descendants_rs {
 	)
 }
 
+=head2 get_descendants()
+
+Returns the node's descendants, if any, as an array reference.
+
+=cut
+
 sub get_descendants { [ shift->get_descendants_rs->all ] }
+
+=head2 get_terminals_rs()
+
+Returns the node's descendant tips, if any, as a L<DBIx::Class> result set.
+
+=cut
 
 sub get_terminals_rs {
 	my $self = shift;
@@ -123,7 +176,19 @@ sub get_terminals_rs {
 	)	
 }
 
+=head2 get_terminals()
+
+Returns the node's descendant tips, if any, as an array reference.
+
+=cut
+
 sub get_terminals { [ shift->get_terminals_rs->all ] }
+
+=head2 get_internals_rs()
+
+Returns the node's descendant internal nodes, if any, as a L<DBIx::Class> result set.
+
+=cut
 
 sub get_internals_rs {
 	my $self = shift;
@@ -139,7 +204,19 @@ sub get_internals_rs {
 	)
 }
 
+=head2 get_internals()
+
+Returns the node's descendant internal nodes, if any, as an array reference.
+
+=cut
+
 sub get_internals { [ shift->get_internals_rs->all ] }
+
+=head2 get_ancestors_rs()
+
+Returns the node's ancestors, if any, as a L<DBIx::Class> result set.
+
+=cut
 
 sub get_ancestors_rs {
 	my $self = shift;
@@ -153,7 +230,19 @@ sub get_ancestors_rs {
 	)
 }
 
+=head2 get_ancestors()
+
+Returns the node's ancestors, if any, as an array ref.
+
+=cut
+
 sub get_ancestors { [ shift->get_ancestors_rs->all ] }
+
+=head2 get_mrca()
+
+Given another node in the same tree, returns the most recent common ancestor of the two.
+
+=cut
 
 sub get_mrca {
 	my ( $self, $other ) = @_;
@@ -206,16 +295,46 @@ sub get_mrca {
 	}
 }
 
+=head2 get_id()
+
+Same as C<id()>, see above.
+
+=cut
+
 sub get_id { shift->id }
+
+=head2 get_name()
+
+Same as C<name()>, see above.
+
+=cut
 
 sub get_name { shift->name }
 
+=head2 get_branch_length()
+
+Same as C<length()>, see above.
+
+=cut
+
 sub get_branch_length { shift->length }
+
+=head2 is_descendant_of()
+
+Given another node, determines of the invocant is the descendant of the argument.
+
+=cut
 
 sub is_descendant_of {
 	my ( $self, $other ) = @_;
 	return ( $self->left > $other->left ) && ( $self->right < $other->right );
 }
+
+=head2 calc_patristic_distance()
+
+Given another node, calculates the patristic distance between the two.
+
+=cut
 
 sub calc_patristic_distance {
 	my ( $self, $other ) = @_;
